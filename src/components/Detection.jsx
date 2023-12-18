69c1f722d6620a3989c82cf4cb671dcfd94c2eb9
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2023-12-18 20:30:40 +0530

    posedetection working but think about minConfidence

--- src/components/Detection.jsx
+++ src/components/Detection.jsx
@@ -1,22 +1,41 @@
 import React, { useRef, useEffect, useState } from "react";
-
+import * as tf from "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs";
-// import * as posenet from "@tensorflow-models/posenet";
+import "@tensorflow/tfjs-backend-webgl";
+import * as poseDetection from "@tensorflow-models/pose-detection";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 
 const Detection = () => {
   const webcamRef = useRef(null);
-  const [score, setScore] = useState(100); // Initial score value
+  // const [score, setScore] = useState(100); // Initial score value
 
   useEffect(() => {
     const runDetection = async () => {
+      // Posenet Architecture
       // const net = await posenet.load({
       //   architecture: "ResNet50",
       //   quantBytes: 2,
       //   inputResolution: { width: 640, height: 480 },
       //   scale: 0.6,
       // });
+      await tf.ready();
+
+      // const detectorConfig = {
+      //   architecture: "MobileNetV1",
+      //   outputStride: 16,
+      //   inputResolution: { width: 640, height: 480 },
+      //   multiplier: 0.75,
+      // };
+
+      const detectorConfig = {
+        modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER,
+      };
+
+      const detector = await poseDetection.createDetector(
+        poseDetection.SupportedModels.MoveNet,
+        detectorConfig
+      );
 
       const objectModel = await cocoSsd.load();
 
@@ -27,26 +46,34 @@ const Detection = () => {
 
         const video = webcamRef.current.video;
 
-        // Pose Detection
+        // Pose Detection using posenet
         // const pose = await net.estimateSinglePose(video);
         // earsDetect(pose.keypoints, 0.8);
+
+        //understand between single pose and multiple poses in tensorflow models
+        const poses = await detector.estimatePoses(video);
+        // if (poses.length > 0) {
+        const firstPose = poses[0];
+        earsDetect(firstPose.keypoints, 0.1);
+        // }
 
         // Object Detection
         const predictions = await objectModel.detect(video);
         handleObjectDetection(predictions);
       };
 
-      // const earsDetect = (keypoints, minConfidence) => {
-      //   const [keypointEarR, keypointEarL] = keypoints.slice(3, 5);
+      const earsDetect = (keypoints, minConfidence) => {
+        const keypointEarL = keypoints[3];
+        const keypointEarR = keypoints[4];
 
-      //   if (
-      //     keypointEarL.score < minConfidence ||
-      //     keypointEarR.score < minConfidence
-      //   ) {
-      //     showNotification("error", "You looked away from the screen");
-      //     updateScore(-15); // Deduct more points when the user looks away
-      //   }
-      // };
+        if (
+          keypointEarL.score < minConfidence ||
+          keypointEarR.score < minConfidence
+        ) {
+          console.log("error", "You looked away from the screen");
+          // updateScore(-15); // Deduct more points when the user looks away
+        }
+      };
 
       const handleObjectDetection = (predictions) => {
         let objectDetected = false;
@@ -59,7 +86,7 @@ const Detection = () => {
 
         if (objectDetected) {
           console.log("error", "Object Detected", "Action has been Recorded");
-          updateScore(-10); // Deduct points when an object is detected
+          // updateScore(-10); // Deduct points when an object is detected
         }
       };
 
@@ -73,12 +100,12 @@ const Detection = () => {
       //   }
       // };
 
-      const updateScore = (points) => {
-        setScore((prevScore) => Math.max(0, prevScore + points));
-      };
+      // const updateScore = (points) => {
+      //   setScore((prevScore) => Math.max(0, prevScore + points));
+      // };
 
-      // Deduct points over time to encourage continuous engagement
-      updateScore(-2);
+      // // Deduct points over time to encourage continuous engagement
+      // updateScore(-2);
 
       setInterval(detect, 500);
     };
@@ -96,6 +123,8 @@ const Detection = () => {
           marginRight: "auto",
           left: 0,
           right: 0,
+          bottom: 0,
+          top: 0,
           textAlign: "center",
           zIndex: 9,
           width: 640,
