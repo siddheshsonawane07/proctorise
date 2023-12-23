74a275ce95e546e58a14f0ca1c389baccee34073
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2023-12-23 21:16:10 +0530

    working can work on time intervals

--- src/components/Detection2.jsx
+++ src/components/Detection2.jsx
@@ -1,32 +1,31 @@
-import React, { useRef, useEffect, useCallback, useState } from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import * as tf from "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl";
+import * as poseDetection from "@tensorflow-models/pose-detection";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 
 const Detection2 = () => {
   const webcamRef = useRef(null);
-  const [faceCount, setFaceCount] = useState(0);
 
-  const handleObjectDetection = useCallback((predictions) => {
-    let count = 0;
+  const handleObjectDetection = useCallback(async (predictions) => {
+    let faceCount = 0;
     predictions.forEach((prediction) => {
       if (prediction.class === "person") {
-        count++;
-      } else if (["cell phone", "book", "laptop"].includes(prediction.class)) {
+        faceCount++;
+      } else if (
+        prediction.class === "cell phone" ||
+        prediction.class === "book" ||
+        prediction.class === "laptop"
+      ) {
         console.log("Object Detected", "Action has been Recorded", "error");
       }
     });
 
-    setFaceCount(count);
-
-    if (count > 1) {
-      console.log(
-        "Multiple Faces Detected",
-        "Please ensure only one face is visible",
-        "error"
-      );
-    } else if (count === 0) {
+    if (faceCount > 1) {
+      console.log("Multiple People Detected", "error");
+    } else if (faceCount === 0) {
       console.log(
         "No Face Detected",
         "Please ensure your face is visible",
@@ -35,12 +34,34 @@ const Detection2 = () => {
     }
   }, []);
 
+  const earsDetect = (keypoints, minConfidence) => {
+    const keypointEarL = keypoints[3];
+    const keypointEarR = keypoints[4];
+
+    if (
+      keypointEarL.score < minConfidence ||
+      keypointEarR.score < minConfidence
+    ) {
+      console.log("error", "You looked away from the screen");
+    }
+  };
+
   useEffect(() => {
     let intervalId;
 
     const runDetection = async () => {
       await tf.ready();
+
       const objectModel = await cocoSsd.load();
+
+      const detectorConfig = {
+        modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER,
+      };
+
+      const detector = await poseDetection.createDetector(
+        poseDetection.SupportedModels.MoveNet,
+        detectorConfig
+      );
 
       const detect = async () => {
         if (!webcamRef.current || webcamRef.current.video.readyState !== 4) {
@@ -52,9 +73,18 @@ const Detection2 = () => {
         // Object Detection
         const predictions = await objectModel.detect(video);
         handleObjectDetection(predictions);
+
+        // Posenet detection
+        const poses = await detector.estimatePoses(video);
+
+        if (poses.length > 0) {
+          earsDetect(poses[0].keypoints, 0.4);
+        } else {
+          console.log("No one detected");
+        }
       };
 
-      intervalId = setInterval(detect, 500);
+      intervalId = setInterval(detect, 5000);
 
       // Cleanup function
       return () => clearInterval(intervalId);
@@ -77,8 +107,13 @@ const Detection2 = () => {
           top: 0,
           textAlign: "center",
           zIndex: 9,
-          width: 640,
-          height: 480,
+          width: 640, // Adjust as needed
+          height: 480, // Adjust as needed
+        }}
+        videoConstraints={{
+          width: 1280, // Set a larger width for a more zoomed-out view
+          height: 720, // Set a larger height for a more zoomed-out view
+          facingMode: "user", // Use "user" for the front camera, "environment" for the rear camera
         }}
       />
     </div>
