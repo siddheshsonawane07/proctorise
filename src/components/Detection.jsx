d660aa984246cc6fe7937674e13f35daa351fa18
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-01-10 23:55:32 +0530

    i hope

--- src/components/Detection.jsx
+++ src/components/Detection.jsx
@@ -1,52 +1,121 @@
-import React, { useRef, useEffect, useCallback, useState } from "react";
-import * as tf from "@tensorflow/tfjs-core";
+import React, { useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
+import Webcam from "react-webcam";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import Webcam from "react-webcam";
-import * as faceapi from "face-api.js";
+import * as tf from "@tensorflow/tfjs-core";
 
 const Detection = () => {
   const webcamRef = useRef(null);
 
-  const handleObjectDetection = useCallback(async (predictions) => {
-    let faceCount = 0;
-
-    predictions.forEach((prediction) => {
-      if (prediction.class === "person") {
-        faceCount++;
-      } else if (["cell phone", "book", "laptop"].includes(prediction.class)) {
-        console.log("Action has been Recorded");
-      }
-    });
-
-    if (faceCount > 1) {
-      console.log("Multiple People Detected");
-    } else if (faceCount === 0) {
-      console.log(
-        "No Face Detected",
-        "Please ensure your face is visible",
-        "error"
-      );
-    }
-  }, []);
-
-  const earsDetect = (keypoints, minConfidence) => {
-    const keypointEarL = keypoints[3];
-    const keypointEarR = keypoints[4];
-
-    if (
-      keypointEarL.score < minConfidence ||
-      keypointEarR.score < minConfidence
-    ) {
-      console.log("You looked away from the screen");
-    }
-  };
-
   useEffect(() => {
-    let intervalId;
+    const getLabeledFaceDescriptions = async () => {
+      console.log("Fetching labeled face descriptions...");
+      const labels = ["siddhesh"];
+      return Promise.all(
+        labels.map(async (label) => {
+          const descriptions = [];
+          const img = await faceapi.fetchImage(
+            "https://firebasestorage.googleapis.com/v0/b/compiler-15a57.appspot.com/o/1.jpg?alt=media&token=d331e175-8acf-4028-9d51-6f72ff6c1062"
+          );
 
-    const runDetection = async () => {
+          console.log("Image fetched");
+          const detections = await faceapi
+            .detectSingleFace(img)
+            .withFaceLandmarks()
+            .withFaceDescriptor();
+          descriptions.push(detections.descriptor);
+          return new faceapi.LabeledFaceDescriptors(label, descriptions);
+        })
+      );
+    };
+
+    const setupFaceRecognition = async (video) => {
+      console.log("Setting up face recognition...");
+      const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+      console.log("Labeled face descriptors:", labeledFaceDescriptors);
+
+      const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+      // setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(video)
+        .withFaceLandmarks()
+        .withFaceDescriptors();
+
+      detections.forEach((detection, i) => {
+        const bestMatch = faceMatcher.findBestMatch(detection.descriptor);
+
+        if (bestMatch.distance < 0.5) {
+          console.log(
+            `Detected face: ${bestMatch.label} (Confidence: ${bestMatch.distance})`
+          );
+        } else {
+          console.log("Face not recognized.");
+        }
+      });
+      // }, 1000);
+    };
+
+    // const detect = async () => {
+    //   if (!webcamRef.current || webcamRef.current.video.readyState !== 4) {
+    //     return;
+    //   }
+
+    //   let video = webcamRef.current.video;
+    // };
+
+    const objectDetection = async (predictions) => {
+      let faceCount = 0;
+
+      predictions.forEach((prediction) => {
+        if (prediction.class === "person") {
+          faceCount++;
+        } else if (
+          ["cell phone", "book", "laptop"].includes(prediction.class)
+        ) {
+          console.log("Action has been Recorded");
+        }
+      });
+
+      if (faceCount > 1) {
+        console.log("Multiple People Detected");
+      } else if (faceCount === 0) {
+        console.log(
+          "No Face Detected",
+          "Please ensure your face is visible",
+          "error"
+        );
+      }
+    };
+
+    const earsDetect = (keypoints, minConfidence) => {
+      const keypointEarL = keypoints[3];
+      const keypointEarR = keypoints[4];
+
+      if (
+        keypointEarL.score < minConfidence ||
+        keypointEarR.score < minConfidence
+      ) {
+        console.log("You looked away from the screen");
+      }
+    };
+
+    const loadModels = async () => {
+      console.log("Loading models...");
       await tf.ready();
+
+      await Promise.all([
+        faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      ]);
+      console.log("Models loaded");
+
+      // Adjust the threshold
+      faceapi.SsdMobilenetv1Options.minConfidence = 0.5;
+
+      await setupFaceRecognition(webcamRef.current.video);
 
       const objectModel = await cocoSsd.load();
 
@@ -59,114 +128,24 @@ const Detection = () => {
         detectorConfig
       );
 
-      const detect = async () => {
-        if (!webcamRef.current || webcamRef.current.video.readyState !== 4) {
-          return;
-        }
+      const predictions = await objectModel.detect(webcamRef.current.video);
+      objectDetection(predictions);
 
-        const video = webcamRef.current.video;
+      // PoseNet detection
+      const poses = await detector.estimatePoses(webcamRef.current.video);
 
-        // Object Detection
-        const predictions = await objectModel.detect(video);
-        handleObjectDetection(predictions);
-
-        // PoseNet detection
-        const poses = await detector.estimatePoses(video);
-
-        if (poses.length > 0) {
-          earsDetect(poses[0].keypoints, 0.5);
-        } else {
-          console.log("No one detected");
-        }
-      };
-
-      loadModelsAndSetupFaceRecognition();
-
-      intervalId = setInterval(detect, 6000);
-
-      // Cleanup functions
-      return () => {
-        clearInterval(intervalId);
-      };
+      if (poses.length > 0) {
+        earsDetect(poses[0].keypoints, 0.5);
+      } else {
+        console.log("No one detected");
+      }
     };
 
-    runDetection();
-  }, [handleObjectDetection]);
-
-  const getLabeledFaceDescriptions = async () => {
-    console.log("Fetching labeled face descriptions...");
-    const labels = ["siddhesh"]; //put your own name
-    return Promise.all(
-      labels.map(async (label) => {
-        const descriptions = [];
-        // const img = await faceapi.fetchImage(`./labels/${label}/1.jpg`);
-
-        //put your own image
-        const img = await faceapi.fetchImage(
-          "https://firebasestorage.googleapis.com/v0/b/compiler-15a57.appspot.com/o/1.jpg?alt=media&token=d331e175-8acf-4028-9d51-6f72ff6c1062"
-        ); // Use a specific image (e.g., 1.jpg)
-
-        console.log("Image fetched");
-        const detections = await faceapi
-          .detectSingleFace(img)
-          .withFaceLandmarks()
-          .withFaceDescriptor();
-        descriptions.push(detections.descriptor);
-        console.log("Labeled face descriptions fetched:", descriptions);
-        return new faceapi.LabeledFaceDescriptors(label, descriptions);
-      })
-    );
-  };
-
-  const setupFaceRecognition = async () => {
-    console.log("Setting up face recognition...");
-    const labeledFaceDescriptors = await getLabeledFaceDescriptions();
-    console.log("Labeled face descriptors:", labeledFaceDescriptors);
-
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
-
-    setInterval(async () => {
-      const detections = await faceapi
-        .detectAllFaces()
-        .withFaceLandmarks()
-        .withFaceDescriptors();
-
-      // Iterate over detected faces
-      detections.forEach((detection, i) => {
-        const bestMatch = faceMatcher.findBestMatch(detection.descriptor);
-
-        // Check if the best match has a certain level of confidence (adjust threshold as needed)
-        if (bestMatch.distance < 0.5) {
-          console.log(
-            `Detected face: ${bestMatch.label} (Confidence: ${bestMatch.distance})`
-          );
-        } else {
-          console.log("Face not recognized.");
-        }
-      });
-    }, 3000);
-  };
-
-  const loadModelsAndSetupFaceRecognition = async () => {
-    console.log("Loading models...");
-    await Promise.all([
-      faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-    ]);
-    console.log("Models loaded");
-
-    // Adjust the threshold
-    faceapi.SsdMobilenetv1Options.minConfidence = 0.5;
-
-    await setupFaceRecognition();
-  };
+    loadModels();
+  }, []);
 
   return (
     <div>
-      <div>
-        <p>The text cannot be copied</p>
-      </div>
       <Webcam
         ref={webcamRef}
         style={{
