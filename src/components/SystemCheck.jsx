1a77bd4ead87a6c31777573290e889c76bd35a20
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-01-29 22:09:59 +0530

    internet speed working

--- src/components/SystemCheck.jsx
+++ src/components/SystemCheck.jsx
@@ -4,7 +4,7 @@ import { ReactInternetSpeedMeter } from "react-internet-meter";
 
 const SystemCheck = () => {
   const [browserInfo, setBrowserInfo] = useState({});
-  const [internetSpeed, setInternetSpeed] = useState(null);
+  const [speedMbps, setSpeedMbps] = useState([0]);
   const [webcamEnabled, setWebcamEnabled] = useState(null);
 
   useEffect(() => {
@@ -27,33 +27,34 @@ const SystemCheck = () => {
       <h1>System Compatibility Check</h1>
       <ul>
         <li>
-          <b>Browser:</b> {browserInfo.name} {browserInfo.version}
+          Browser: {browserInfo.name} {browserInfo.version}
         </li>
         <li>
-          <b>Webcam:</b>{" "}
+          Webcam:{" "}
           {webcamEnabled !== null
             ? webcamEnabled
               ? "Enabled"
               : "Disabled"
             : "Checking..."}
         </li>
-        <ReactInternetSpeedMeter
-          txtSubHeading="Internet connection is slow"
-          outputType="" // "alert"/"modal"/"empty"
-          customClassName={null}
-          pingInterval={2000} // milliseconds
-          txtMainHeading="Opps..."
-          thresholdUnit="megabyte" // "byte" , "kilobyte", "megabyte"
-          threshold={50}
-          downloadSize="1561257" // bytes
-          callbackFunctionOnNetworkDown={(data) =>
-            console.log(`Internet speed : ${data}`)
-          }
-          callbackFunctionOnNetworkTest={(data) => setInternetSpeed(data)}
-        />
-        <li>
-          <b>Internet Speed:</b> {internetSpeed}
-        </li>
+        <div>
+          <ReactInternetSpeedMeter
+            txtSubHeading="Internet connection is slow"
+            outputType="" // "alert"/"modal"/"empty"
+            customClassName={null}
+            pingInterval={2000} // milliseconds
+            txtMainHeading="Opps..."
+            thresholdUnit="megabyte" // "byte" , "kilobyte", "megabyte"
+            threshold={50}
+            imageUrl="https://i.postimg.cc/sft772VP/speedometer.png"
+            downloadSize="1561257" //bytes
+            callbackFunctionOnNetworkDown={(data) =>
+              console.log(`Internet speed : ${data}`)
+            }
+            callbackFunctionOnNetworkTest={(data) => setSpeedMbps(data)}
+          />
+        </div>
+        <li>Internet Speed: {speedMbps} MB/s</li>
       </ul>
     </div>
   );
