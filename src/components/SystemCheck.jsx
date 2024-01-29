7a31554ed07e8aad1e6ae31ebbcfb9eaf0363631
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-01-29 22:45:01 +0530

    webcam issue sorted

--- src/components/SystemCheck.jsx
+++ src/components/SystemCheck.jsx
@@ -1,26 +1,57 @@
-import React, { useEffect, useState } from "react";
-import DetectRTC from "detectrtc";
+import React, { useEffect, useState, useRef } from "react";
 import { ReactInternetSpeedMeter } from "react-internet-meter";
+import DetectRTC from "detectrtc";
 
 const SystemCheck = () => {
   const [browserInfo, setBrowserInfo] = useState({});
   const [speedMbps, setSpeedMbps] = useState([0]);
-  const [webcamEnabled, setWebcamEnabled] = useState(null);
+  const [webcamEnabled, setWebcamEnabled] = useState("Checking...");
+  const videoRef = useRef(null);
 
   useEffect(() => {
-    // Function to check browser info and webcam permissions
     const checkSystemInfo = () => {
       setBrowserInfo({
         name: DetectRTC.browser.name,
         version: DetectRTC.browser.version,
       });
 
-      setWebcamEnabled(DetectRTC.isWebsiteHasWebcamPermissions);
+      // Check webcam permissions using navigator.mediaDevices.getUserMedia
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(() => setWebcamEnabled(true))
+        .catch(() => setWebcamEnabled(false));
     };
 
-    // Call the function when the component mounts
     checkSystemInfo();
-  }, []); // Empty dependency array ensures that this effect runs only once on mount
+  }, []);
+
+  useEffect(() => {
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error("Error accessing camera:", error);
+      }
+    };
+
+    startCamera();
+
+    return () => {
+      if (videoRef.current) {
+        const stream = videoRef.current.srcObject;
+        if (stream) {
+          const tracks = stream.getTracks();
+          tracks.forEach((track) => track.stop());
+        }
+      }
+    };
+  }, []);
 
   return (
     <div>
@@ -29,31 +60,24 @@ const SystemCheck = () => {
         <li>
           Browser: {browserInfo.name} {browserInfo.version}
         </li>
-        <li>
-          Webcam:{" "}
-          {webcamEnabled !== null
-            ? webcamEnabled
-              ? "Enabled"
-              : "Disabled"
-            : "Checking..."}
-        </li>
+        <li>Webcam: {webcamEnabled === true ? "Enabled" : "Disabled"}</li>
         <div>
           <ReactInternetSpeedMeter
             txtSubHeading="Internet connection is slow"
-            outputType="" // "alert"/"modal"/"empty"
+            outputType=""
             customClassName={null}
-            pingInterval={2000} // milliseconds
+            pingInterval={2000}
             txtMainHeading="Opps..."
-            thresholdUnit="megabyte" // "byte" , "kilobyte", "megabyte"
+            thresholdUnit="megabyte"
             threshold={50}
             imageUrl="https://i.postimg.cc/sft772VP/speedometer.png"
-            downloadSize="1561257" //bytes
-            // callbackFunctionOnNetworkDown={}
+            downloadSize="1561257"
             callbackFunctionOnNetworkTest={(data) => setSpeedMbps(data)}
           />
         </div>
         <li>Internet Speed: {speedMbps} MB/s</li>
       </ul>
+      <video ref={videoRef} width="480" height="480" autoPlay></video>
     </div>
   );
 };
