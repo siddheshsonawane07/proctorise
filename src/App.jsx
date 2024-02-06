d46dda1e14851ea1562b2b5efcc5b094c7370cb1
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-02-06 21:34:19 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Home from "./components/Home";
 import Home2 from "./components/Home2";
 import SystemCheck from "./components/SystemCheck";
 import DetectionCheck from "./components/DetectionCheck";
+import TestPage from "./components/TestPage";
 
 class App extends Component {
   render() {
@@ -23,6 +24,7 @@ class App extends Component {
           <Route path="/home" element={<Home2 />} />
           <Route path="/systemcheck" element={<SystemCheck />} />
           <Route path="/detectioncheck" element={<DetectionCheck />} />
+          <Route path="/test" element={<TestPage />} />
           <Route path="/404" element={<PageNotFound />} />
           <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -1,8 +1,34 @@
-import { Button } from "@material-ui/core";
+import React, { useRef, useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { useNavigate } from "react-router-dom";
+import { auth, app } from "../utils/firebase-config";
+import { Button } from "@material-ui/core";
 
 const Home2 = () => {
+  const [user] = useAuthState(auth);
+  const webcamRef = useRef(null);
+  const [hasStorageRef, setHasStorageRef] = useState(false);
+  const [imageLink, setimageLink] = useState(null);
+  const storage = getStorage(app);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkStorageRef = async () => {
+      const storageRef = ref(storage, `/images/${user.email}`);
+      try {
+        const imageLink = await getDownloadURL(storageRef);
+        setHasStorageRef(true);
+        setimageLink(imageLink);
+      } catch (error) {
+        setHasStorageRef(false);
+      }
+    };
+
+    if (user) {
+      checkStorageRef();
+    }
+  }, [user, storage]);
 
   const handleSystemCheck = () => {
     navigate("/systemcheck");
@@ -31,6 +57,12 @@ const Home2 = () => {
       </div>
       <Button onClick={handleDetectionCheck}>Check the basic detections</Button>
       <Button onClick={handleTestButton}> Test Page</Button>
+      <div>
+        <p>hasStorageRef{hasStorageRef} </p>
+      </div>
+      <div>
+        <p>imageLink{imageLink} </p>
+      </div>
     </div>
   );
 };
