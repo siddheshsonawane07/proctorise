ba8f60a6287df7ab89cf660c65572f6aefcd2a33
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-04-06 13:45:19 +0530

    project config

--- src/utils/firebase-config.jsx
+++ src/utils/firebase-config.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6JzEHEbGTiH1UE0mJ-cj9cfz3gUYH6YY",
-  authDomain: "proctorise-9e9b9.firebaseapp.com",
-  projectId: "proctorise-9e9b9",
-  storageBucket: "proctorise-9e9b9.appspot.com",
-  messagingSenderId: "34275606143",
-  appId: "1:34275606143:web:95545bfd0b8c5b0026c94a",
+  apiKey: "AIzaSyBHlMzEEbgHkJq2jWXO8_2P4-ehsDN48XY",
+  authDomain: "proctorise-22190.firebaseapp.com",
+  projectId: "proctorise-22190",
+  storageBucket: "proctorise-22190.appspot.com",
+  messagingSenderId: "57986258213",
+  appId: "1:57986258213:web:a98d655dcd3ff92a7cb6cc"
 };
 
 const app = initializeApp(firebaseConfig);
